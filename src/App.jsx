034c336490e8694b685027f5b4be4f22d86761c8
RichardPFernandes <fernandesrichard312@gmail.com>
2024-08-27 21:30:59 -0300

    feat(card): finalizado card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import React from 'react'
 import Card from './components/card/Card'
 import Navbar from './components/navbar'
 import html from './assets/html.png'
 import js from './assets/js.png'
+import css from './assets/css.png'
 
 function App() {
+  const [cards] = React.useState([
+    { id: 1, imagem: html },
+    { id: 2, imagem: js },
+    { id: 3, imagem: css },
+  ]);
+
   return (
     <>
       <div>
@@ -14,9 +22,9 @@ function App() {
             <h1>Qual a Sua Escolha?</h1>
           </div>
           <div className='carousel'>
-            <Card imagem={html} />
-            <Card imagem={js} />
-            <Card />
+            {cards.map((card) => (
+              <Card key={card.id} imagem={card.imagem} />
+            ))}
           </div>
         </main>
       </div>
